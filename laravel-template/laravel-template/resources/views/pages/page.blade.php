085c7a8983428d68808222ae620f1741d45febb6
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-22 21:58:11 +0200

    cloned template, ready to use

--- laravel-template/laravel-template/resources/views/pages/page.blade.php
+++ laravel-template/laravel-template/resources/views/pages/page.blade.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> @yield("page-title", "") </title>
 
+
+    @vite("resources/js/app.js")
 </head>
 <body>
 
+<h1 class="text-danger text-center">Homepage</h1>
 
 
 
-
-    <title> @yield("page-title", "") </title>
 </body>
 </html>
